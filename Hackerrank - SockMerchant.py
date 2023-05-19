820b332d46fca950ffb8c27dd29fe9bf938a8f74
baris <barissever1@outlook.com>
2023-05-19 11:11:26 +0300

    Hackerrank SockMerchant Solution

---
+++ Hackerrank - SockMerchant.py
@@ -0,0 +1,20 @@
+count = int(input("How many socks are there? "))
+while True:
+    color_list = list(map(int, input("Enter the socks colors: ").rstrip().split()))
+    if len(color_list) != count:
+        print("You have entered wrong number of socks, please enter again.")
+        continue
+    else:
+        arr = []
+        for i in set(color_list):
+            ct = 0
+            for a in color_list:
+                if a == i:
+                    ct += 1
+            arr.append(ct)
+        break
+
+result = 0
+for i in arr:
+   result += ((i - (i % 2)) / 2)
+print(result)
