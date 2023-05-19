48980a2ffa5a5936085d8b6b99bb132019cfc432
baris <barissever1@outlook.com>
2023-05-19 11:47:58 +0300

    PasswordCheck Corrected

--- PasswordCheck.py
+++ PasswordCheck.py
@@ -1,26 +1,28 @@
-password = input("Enter your password: ")
-pass_list = list(password)
-check = 0
-if len(pass_list) < 16:
-    print("Your password should be at least 16 letters")
-else:
-    for i in pass_list:
-        if i.isnumeric() == True:
-            check = 1
-    if check == 0:
-        print("Your password should include at least one numeric value")
+while True:
+    password = input("Enter your password: ")
+    pass_list = list(password)
+    check = 0
+    if len(pass_list) < 16:
+        print("Your password should be at least 16 letters")
     else:
         for i in pass_list:
-            if i.isupper() == True:
-                check = 2
-        if check == 1:
-            print("Your password should include at least one uppercase letter")
+            if i.isnumeric() == True:
+                check = 1
+        if check == 0:
+            print("Your password should include at least one numeric value")
         else:
             for i in pass_list:
-                if i.isalnum() == False:
-                    check = 3
-            if check == 2:
-                print("Your password should include at least one non-alphanumeric letter")
+                if i.isupper() == True:
+                    check = 2
+            if check == 1:
+                print("Your password should include at least one uppercase letter")
             else:
-                print("Your password is true")
+                for i in pass_list:
+                    if i.isalnum() == False:
+                        check = 3
+                if check == 2:
+                    print("Your password should include at least one non-alphanumeric letter")
+                else:
+                    print("Your password is true")
+                    break
 
