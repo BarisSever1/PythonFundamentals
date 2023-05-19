b4ec1e585f368775298f74b546ee41ffd43ca558
baris <barissever1@outlook.com>
2023-05-19 17:03:10 +0300

    Hackerrank Climbing The Leaderboard Solution

--- Hackerrank - ClimbingTheLeaderboard.py
+++ Hackerrank - ClimbingTheLeaderboard.py
@@ -5,8 +5,12 @@ while True:
     ranked = list(map(int, input("Enter ranked points: ").rstrip().split()))
     game_count = int(input("How many games? ").strip())
     games = list(map(int, input("Enter game points: ").rstrip().split()))
-    if ranked_count != len(ranked) or game_count != len(games):
-        print("Wrong ranked or player count, enter again!")
+    ranked = sorted(ranked, reverse=True)
+    player = sorted(games, reverse=True)
+    if ranked_count != len(ranked):
+        print("Wrong ranked count, enter again!")
+    elif game_count != len(games):
+        print("Wrong game count, enter again!")
     else:
         for i in games:
             ct = 1
